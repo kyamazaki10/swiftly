22e1e6468a52e9ba539bfae0e7c0be5b71a23569
Kumiko Yamazaki <kyamazaki@gmail.com>
2018-03-27 18:46:18 -0600

    target-action pattern 🎯

--- Swiftly/ViewController.swift
+++ Swiftly/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var locationTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        locationLabel.text = "Default Text"
+    }
 }
 
