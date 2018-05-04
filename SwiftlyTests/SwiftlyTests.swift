ecf5708caa414181a45feecab2a8b9457273b018
Kumiko Yamazaki <kyamazaki@gmail.com>
2018-05-04 07:44:50 -0600

    basic Data Model unit tests

--- SwiftlyTests/SwiftlyTests.swift
+++ SwiftlyTests/SwiftlyTests.swift
@@ -9,27 +9,32 @@ import XCTest
 @testable import Swiftly
 
 class SwiftlyTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    //MARK: Location Class Tests
+
+    // Confirm that the Location initializer returns a Location object when passed valid parameters.
+    func testLocationInitializationSucceeds() {
+        // Zero rating.
+        let zeroRatingLocation = Location.init(name: "Zero", rating: 0, photo: nil)
+        XCTAssertNotNil(zeroRatingLocation)
+
+        // Highest positive rating.
+        let positiveRatingLocation = Location.init(name: "Positive", rating: 5, photo: nil)
+        XCTAssertNotNil(positiveRatingLocation)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    // Confirm that the Location initializer returns nil when passed a negative rating or an empty name.
+    func testLocationInitializationFails() {
+        // Negative rating.
+        let negativeRatingLocation = Location.init(name: "Negative", rating: -1, photo: nil)
+        XCTAssertNil(negativeRatingLocation)
+
+        // Exceeds maximum rating.
+        let largeRatingLocation = Location.init(name: "Large", rating: 6, photo: nil)
+        XCTAssertNil(largeRatingLocation)
+
+        // Empty string.
+        let emptyStringLocation = Location.init(name: "", rating: 0, photo: nil)
+        XCTAssertNil(emptyStringLocation)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
